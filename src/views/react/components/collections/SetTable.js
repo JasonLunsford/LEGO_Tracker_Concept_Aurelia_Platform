99840dccb5be90b18cd935a4ed27bc2152e849eb
JasonLunsford <jl630043@gmail.com>
2018-06-30 08:38:40 -0400

    early version of table search

--- src/views/react/components/collections/SetTable.js
+++ src/views/react/components/collections/SetTable.js
@@ -7,6 +7,11 @@ import {Table, Header, Body, SmallCell, I} from './styles/collections.sc';
 
 export default class SetTable extends Component {
 
+    state = {
+        members: this.props.members,
+        query: ''
+    }
+
     componentWillMount() {
         _.map(this.props.members, member => {
             member.theme = this.getTheme(this.props.themes, member.theme_id);
@@ -30,10 +35,37 @@ export default class SetTable extends Component {
         return minifigs;
     }
 
+    handleSearch() {
+        if (!_.isEmpty(this.search.value)) {
+            let searchValue = _.toLower(this.search.value)
+            _.delay(() => {
+                let members = _.filter(this.props.members, member => {
+                    let searchName = _.toLower(member.name);
+                    return _.includes(searchName, searchValue);
+                });
+
+                this.setState({
+                    members,
+                    query: this.search.value
+                });
+            }, 1000);
+        } else {
+            this.setState({
+                members: this.props.members,
+                query: ''
+            });
+        }
+    }
+
     render() {
-        const { members, themes } = this.props;
+        const { themes } = this.props;
 
         return (
+            <div>
+            <input placeholder="Search..."
+                   ref={input => this.search = input}
+                   onChange={e => this.handleSearch()}
+            />
             <Table> 
                 <Header>
                     <SmallCell thickleft><span>Name</span></SmallCell>
@@ -49,7 +81,7 @@ export default class SetTable extends Component {
                     <SmallCell thickright><span>View</span></SmallCell>
                 </Header>
                 <Body>
-                {members.map((member, index) => 
+                {this.state.members.map((member, index) => 
                     <div key={index}>
                         <SmallCell thickleft><span title={member.name}>{member.name}</span></SmallCell>
                         <SmallCell><span>{member.set_num}</span></SmallCell>
@@ -66,6 +98,7 @@ export default class SetTable extends Component {
                 )}
                 </Body>
             </Table>
+            </div>
         );
     }
 }
